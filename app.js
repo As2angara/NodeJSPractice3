861bb56d6254c14f9d9c6a420cf1ad6cbd9a9708
AdrianSA <Adrian.angara@gmail.com>
2020-03-31 15:17:18 -0400

    added use cases for comparison operators, logical operators, and regular expressions

--- app.js
+++ app.js
@@ -27,6 +27,27 @@ async function createCourse() {
     console.log(result);
 }
 
+//List of comparison operators
+//eq (equal)
+//ne (not equal)
+//gt (greater than)
+//gte (greater than or equal)
+//lt (less than)
+//lte (less than or equal)
+//in
+//nin (not in)
+    //ex: .find({ price: { $gte: 10, $lte: 20 } })
+    //Find price, p that is  10 <= p , and p <= 20
+    //ex: .find({ price: { $in: [ 10, 20, 30 ] } });
+    //Find price that corresponds the an item in the list
+
+//Logical Operators
+//or()
+//and()
+    //ex: .find().or([ { name: 'Kid' }, { isPublished: true } ])
+    //Find course with name 'Kid', OR isPublished is true
+
+
 async function getCourses() {
     const courses = await Course
             .find({ author: 'Adrian Angara', isPublished: true })
